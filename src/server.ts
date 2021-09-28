d3336c23d4fb997bbe2265b69abe1b148f161625
Jadir Junior <jadirjsjunior@gmail.com>
2021-09-28 09:56:04 -0300

    :wrench: configuration the server

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import * as http from 'http';
+
+import express, { Application } from 'express';
+
+import cors from 'cors';
+import expressPino from 'express-pino-logger';
+import logger from './logger';
+
+export class SetupServer {
+  private server?: http.Server;
+  private app: Application;
+
+  constructor(private port = 3000) {}
+
+  public async init(): Promise<void> {
+    this.setupExpress();
+  }
+
+  private setupExpress(): void {
+    this.app.use(express.json());
+    this.app.use(
+      expressPino({
+        logger,
+      })
+    );
+    this.app.use(
+      cors({
+        origin: '*',
+      })
+    );
+  }
+
+  public start(): void {
+    this.server = this.app.listen(this.port, () => {
+      logger.info('Server listing on port: ' + this.port);
+    });
+  }
+
+  public async close(): Promise<void> {}
+}
