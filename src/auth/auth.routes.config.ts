5a9e4a421ce3330ed9a7de0caa59018146dd27c7
Jadir Junior <jadirjsjunior@gmail.com>
2021-10-05 11:26:24 -0300

    :wrench: remove module-alias

--- src/auth/auth.routes.config.ts
+++ src/auth/auth.routes.config.ts
@@ -1,9 +1,9 @@
 import { Application } from 'express';
-import { CommonRoutesConfig } from '@src/common/common.routes.config';
+import { CommonRoutesConfig } from '../common/common.routes.config';
 import authController from './controller/auth.controller';
 import authMiddleware from './middleware/auth.middleware';
 import { body } from 'express-validator';
-import bodyValidationMiddleware from '@src/common/middleware/body.validation.middleware';
+import bodyValidationMiddleware from '../common/middleware/body.validation.middleware';
 
 export class AuthRoutes extends CommonRoutesConfig {
     constructor(app: Application) {
