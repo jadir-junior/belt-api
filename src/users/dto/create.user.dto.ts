4642f99f42a5d062bf57168eb4398598cb01f102
Jadir Junior <jadirjsjunior@gmail.com>
2021-10-19 16:31:03 -0300

    :construction: create dto put user

--- src/users/dto/create.user.dto.ts
+++ src/users/dto/create.user.dto.ts
@@ -2,5 +2,6 @@ export interface CreateUserDto {
     email: string;
     password: string;
     name?: string;
+    position?: string;
     permissionFlags?: number;
 }
---
+++ src/users/dto/put.user.dto.ts
@@ -0,0 +1,8 @@
+export interface PutUserDto {
+    _id: string;
+    email: string;
+    password: string;
+    name: string;
+    position: string;
+    permissionFlags: number;
+}
